263e17e313c8d44230c376f94d6f76b0d794b676
Ryan <ryantruong927@gmail.com>
2022-10-01 02:23:30 -0400

    Added simple rooms, doors, and camera movement
    There is a bug with the tiles changing sprites due to using prefabs incorrectly. This will be fixed later.
    
    - renamed project
    - forgot to remove Demo.meta

---
+++ Assets/Prefabs/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject startingRoom;
+	private GameObject currentRoom;
+	public GameObject CurrentRoom { get { return currentRoom; } }
+
+	private Vector3 targetPosition;
+
+	private bool isMoving;
+	public float moveTime = 1f;
+
+	private void Awake() {
+		MoveCamera(startingRoom, startingRoom.transform.position);
+		targetPosition = transform.position;
+		isMoving = false;
+	}
+
+	private void Update() {
+		if (isMoving) {
+			transform.position = Vector3.MoveTowards(transform.position, targetPosition, Vector3.Distance(transform.position, targetPosition) / moveTime * Time.unscaledDeltaTime);
+
+			Time.timeScale = 0;
+
+			if (Mathf.Abs(Vector3.Distance(transform.position, targetPosition)) <= 0.05f) {
+				Time.timeScale = 1;
+				transform.position = targetPosition;
+				isMoving = false;
+			}
+		}
+	}
+
+	public void MoveCamera(GameObject newRoom, Vector3 position) {
+		currentRoom = newRoom;
+		targetPosition = new Vector3(position.x, position.y, -10);
+		isMoving = true;
+	}
+}
---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,48 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	public GameObject room1, room2;
+	private GameObject currentRoom;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.GetComponent<RubyController>() != null) {
+			Debug.Log("Entered");
+			Vector3 position = Camera.main.transform.position;
+
+			if (position.x == room1.transform.position.x && position.y == room1.transform.position.y) {
+				Camera.main.GetComponent<CameraController>().MoveCamera(room2, room2.transform.position);
+				currentRoom = room2;
+			}
+			else {
+				Camera.main.GetComponent<CameraController>().MoveCamera(room1, room1.transform.position);
+				currentRoom = room1;
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+		RubyController ruby = collision.GetComponent<RubyController>();
+
+		if (ruby != null) {
+			Debug.Log("Exited");
+			Vector3 rubyPosition = ruby.transform.position;
+			Vector3 room1Position = room1.transform.position;
+			Vector3 room2Position = room2.transform.position;
+
+			if (Camera.main.GetComponent<CameraController>().CurrentRoom == room1) {
+				if (Vector3.Distance(rubyPosition, room1Position) > Vector3.Distance(rubyPosition, room2Position)) {
+					Camera.main.GetComponent<CameraController>().MoveCamera(room2, room2Position);
+				}
+			}
+			else {
+				if (Vector3.Distance(rubyPosition, room2Position) > Vector3.Distance(rubyPosition, room1Position)) {
+					Camera.main.GetComponent<CameraController>().MoveCamera(room1, room1Position);
+				}
+			}
+		}
+	}
+}
+
